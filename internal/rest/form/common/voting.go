ac0ee3ab6cc86572699af7f8b03046c153120a8e
Alexander Kirii <nwn.wert@gmail.com>
2023-11-10 18:52:06 +0500

    Add voting endpoints

---
+++ internal/rest/form/common/voting.go
@@ -0,0 +1,43 @@
+package common
+
+import (
+	"encoding/json"
+
+	"github.com/goverland-labs/core-web-api/internal/response"
+	"github.com/goverland-labs/core-web-api/internal/response/errs"
+)
+
+type Voter string
+
+func (v *Voter) ValidateAndSet(voter string, errors map[string]response.ErrorMessage) {
+	if voter == "" {
+		errors["voter"] = response.ErrorMessage{
+			Code:    errs.MissedValue,
+			Message: "missing voter",
+		}
+	}
+
+	*v = Voter(voter)
+}
+
+type Choice json.RawMessage
+
+func (c *Choice) ValidateAndSet(choice json.RawMessage, errors map[string]response.ErrorMessage) {
+	if choice == nil || len(choice) == 0 {
+		errors["choice"] = response.ErrorMessage{
+			Code:    errs.MissedValue,
+			Message: "missing choice",
+		}
+	}
+
+	var skip any
+	err := json.Unmarshal(choice, &skip)
+	if err != nil {
+		errors["choice"] = response.ErrorMessage{
+			Code:    errs.WrongFormat,
+			Message: "choice has wrong format",
+		}
+	}
+
+	*c = Choice(choice)
+}
